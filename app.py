852df8659c60eef266995bd10a0488edb1a4ff0b
Shivam Tripathi <97305325+shivam0975@users.noreply.github.com>
2025-08-04 20:22:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -53,4 +53,4 @@ def submit():
     return render_template('submit.html', prediction=round(prediction, 3))
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5000, debug=True)
